e342834fe613d6c40dfabeb3add15ca672031a74
Luis Rogelio Hernández López <Kurtz@Luiss-MacBook-Pro.local>
2016-11-23 00:38:15 -0600

    Add restriction to directive template

--- templates/directive/_name.directive.ts
+++ templates/directive/_name.directive.ts
@@ -1,7 +1,8 @@
 import { <%= pName %>Controller } from "./<%= hName %>.controller.ts";
 
 let <%= name %>Directive = (): ng.IDirective => {
-    return {
+    return {<% if (restrict) { %>
+        restrict: "<%= restriction %>",<% } %>
         bindToController: true,
         controllerAs: "<%= ctrlAlias %>",
         controller: <%= pName %>Controller
