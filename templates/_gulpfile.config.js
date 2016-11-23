433e1543515a9bf804cf064601845513e94e9097
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-22 20:37:21 -0600

    Add gulpfile.config template

---
+++ templates/_gulpfile.config.js
@@ -0,0 +1,27 @@
+module.exports = function () {
+    var outputPath = "./<%= outFolder %>/";
+    var src = "./<%= srcFolder %>/";
+
+    var config = {
+        vendor: {
+            destPath: outputPath + "js",
+            src: []
+        },
+        templates: src + "**/*.tpl.html",
+        scss: {
+            entry: src + "styles/<%= appName %>.scss",
+            src: [src + "**/*.scss"],
+            destPath: outputPath + "css"
+        },
+        ts: {
+            src: [src + "**/*.ts"],
+            dest: outputPath + "js"
+        },
+        html: {
+            src: [src + "**/**.html"],
+            dest: outputPath + "js"
+        }
+    };
+
+    return config;
+};
