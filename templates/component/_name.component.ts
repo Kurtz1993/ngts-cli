591a7b25ead5d98e7e4616eb9df39986326be1d0
Luis Rogelio Hernández López <Kurtz@Luiss-MacBook-Pro.local>
2016-11-23 01:40:39 -0600

    Add tplPath to templateUrl

--- templates/component/_name.component.ts
+++ templates/component/_name.component.ts
@@ -3,7 +3,7 @@ import { <%= pName %>Controller } from "./<%= hName %>.controller";
 let <%= name %>: ng.IComponentOptions = {
     controller: <%= pName %>Controller,
     controllerAs: "<%= ctrlAlias %>",
-    templateUrl: "<%= hName %>/<%= hName %>.tpl.html",
+    templateUrl: "<%= tplPath %><%= hName %>.tpl.html",
     bindings: {
 
     }
