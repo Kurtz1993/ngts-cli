9eb452d04e3ecd011aedb7308a83a072c5e4b0ab
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-11-22 21:10:28 -0600

    Add controller template

---
+++ templates/controller/_name.ts
@@ -0,0 +1,9 @@
+import { Inject } from "<%= decoratorPath %>";
+
+@Inject()
+export class <%= pAppName %>Config {
+    constructor() { }
+
+    /** Initializes the controller. */
+    public $onInit(): void {}
+}
