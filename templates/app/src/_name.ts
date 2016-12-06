dac59c1b547963dd2e23c7ffc86ebacfa2a68bee
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-12-06 15:05:37 -0600

    Add tpls module to default scaffolded module

--- templates/app/src/_name.ts
+++ templates/app/src/_name.ts
@@ -2,6 +2,7 @@ import { <%= pAppName %>Config } from "./<%= hAppName %>.config";
 import { <%= pAppName %>Run } from "./<%= hAppName %>.run";
 
 angular.module("<%= appName %>", [
+    "<%= appName %>.tpls",
     "ui.router"
 ])
 .config(<%= pAppName %>Config)
