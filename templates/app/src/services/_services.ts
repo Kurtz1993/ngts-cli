4f96ab479b989477886c5df9dd42fb03a38b7a97
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-01-26 17:53:42 -0600

    Update _services.ts

--- templates/app/src/services/_services.ts
+++ templates/app/src/services/_services.ts
@@ -1,6 +1,3 @@
-let module = angular
-    .module("<%= appName %>.services", []);
-
-let services = module.name;
-
-export { services };
+export const services = angular
+    .module("<%= appName %>.services", [])
+    .name;
