d2735d894ef36196929fa89b23bdc13f6f11a850
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-01-26 17:51:55 -0600

    Update _name.ts

--- templates/module/_name.ts
+++ templates/module/_name.ts
@@ -1,11 +1,8 @@
 <% if (!moduleOnly) { %>import { <%= pName %>Config } from "./<%= hName %>.config";
 import { <%= pName %>Controller } from "./<%= hName %>.controller";
 
-<% } %>let module = angular
+<% } %>export const <%= name %> = angular
     .module("<%= appName %>.<%= name %>", [])<% if (!moduleOnly) { %>
     .controller("<%= pName %>Controller", <%= pName %>Controller)
-    .config(<%= pName %>Config)<% } %>;
-
-let <%= name %> = module.name;
-
-export { <%= name %> };
+    .config(<%= pName %>Config)<% } %>
+    .name;
