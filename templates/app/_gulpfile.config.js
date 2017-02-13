6372c226095d5cbe8e6081f0efda081a694c7063
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-02-13 13:12:42 -0600

    Remove vendor and ts properties
    Will be handled by webpack.

--- templates/app/_gulpfile.config.js
+++ templates/app/_gulpfile.config.js
@@ -3,22 +3,11 @@ module.exports = function () {
     var src = "./<%= srcFolder %>/";
 
     var config = {
-        vendor: {
-            destPath: outputPath + "js",
-            src: [
-                "node_modules/angular/angular.js",
-                "node_modules/angular-ui-router/release/angular-ui-router.js"
-            ]
-        },
         templates: src + "**/*.tpl.html",
         scss: {
             entry: src + "styles/<%= hAppName %>.scss",
             src: [src + "**/*.scss"],
             destPath: outputPath + "css"
-        },
-        ts: {
-            src: [src + "**/*.ts"],
-            dest: outputPath + "js"
         },
         html: {
             src: [src + "**/**.html"],
