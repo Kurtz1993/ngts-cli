4894480b94d566bf138ab006f9ba9125f1d86efa
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-02-13 13:12:53 -0600

    Remove ts and vendor builds
    Will be handled by webpack

--- templates/app/_gulpfile.js
+++ templates/app/_gulpfile.js
@@ -1,34 +1,9 @@
 const gulp = require("gulp");
 const templateCache = require("gulp-angular-templatecache");
-const concat = require("gulp-concat");
 const sass = require("gulp-sass");
-const webpack = require("webpack-stream");
-const webpackConf = require("./<%= srcFolder %>/webpack.config");
 
 // Gulp configuration file.
 const config = require("./gulpfile.config")();
-
-/**
- * Builds TypeScript sources.
- * @param {boolean} watch Specifies if the build should occur in watch mode.
- */
-function buildTypescript(watch) {
-    return function() {
-        webpackConf.watch = watch;
-        return gulp
-            .src("./<%= srcFolder %>/<%= hAppName %>.ts")
-            .pipe(webpack(webpackConf))
-            .pipe(gulp.dest(config.ts.dest));
-    }
-}
-
-// Concatenate all vendor components into a single file.
-gulp.task("bundle-vendors", function() {
-    return gulp
-        .src(config.vendor.src)
-        .pipe(concat("vendors.js"))
-        .pipe(gulp.dest(config.vendor.destPath));
-});
 
 gulp.task("sass", function() {
     return gulp.src(config.scss.entry)
@@ -43,21 +18,10 @@ gulp.task("bundle-tpls", function() {
 });
 
 // Watches for changes in application"s files
-gulp.task("watch", ["bundle-vendors", "build-ui"], function(done) {
-    gulp.watch(config.scss.src, ["css"]);
-    gulp.watch(config.html.src, ["bundle-tpls"]);
-    buildTypescript(true)();
-});
-
-// Watches for changes in application"s files except for ts/js files.
-gulp.task("watch-ui", ["build-ui"], function() {
+gulp.task("watch", ["build-ui"], function(done) {
     gulp.watch(config.scss.src, ["css"]);
     gulp.watch(config.html.src, ["bundle-tpls"]);
 });
 
-// Bundle all the application files.
-gulp.task("build", ["bundle-ts", "bundle-vendors", "build-ui"]);
-// Bundle all TS app files.
-gulp.task("bundle-ts", buildTypescript(false));
 // Bundle HTML and SCSS files.
 gulp.task("build-ui", ["sass", "bundle-tpls"]);
