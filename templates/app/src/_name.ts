0c93fe50c7d234b54d8ce2db4ba25b06bc143f45
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-02-13 13:14:09 -0600

    Add services and angular reference

--- templates/app/src/_name.ts
+++ templates/app/src/_name.ts
@@ -1,10 +1,13 @@
+import * as angular from "angular";
 import { <%= pAppName %>Config } from "./<%= hAppName %>.config";
 import { <%= pAppName %>Run } from "./<%= hAppName %>.run";
+import { services } from "./services/services";
 
 angular.module("<%= appName %>", [
     // Uncomment to use your app templates.
     // "<%= appName %>.tpls",
-    "ui.router"
+    "ui.router",
+    services
 ])
 .config(<%= pAppName %>Config)
 .run(<%= pAppName %>Run);
