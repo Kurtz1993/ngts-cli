c52bf2ab1b43f03655fd0bd809c14d65c8ecc459
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-02-13 13:14:42 -0600

    Use .module and add references

--- templates/module/_name.module.ts
+++ templates/module/_name.module.ts
@@ -1,6 +1,9 @@
+import * as angular from "angular";
 <% if (!moduleOnly) { %>import { <%= pName %>Config } from "./<%= hName %>.config";
+import { <%= pName %>Controller } from "./<%= hName %>.controller";
 
 <% } %>export const <%= name %> = angular
     .module("<%= appName %>.<%= name %>", [])<% if (!moduleOnly) { %>
+    .controller("<%= pName %>", <%= pName %>Controller)
     .config(<%= pName %>Config)<% } %>
     .name;
