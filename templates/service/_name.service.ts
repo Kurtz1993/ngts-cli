a3f40f0327691441efeec703df0663e0bbb364f0
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-03-08 10:23:56 -0600

    Add interface to service as exported member

--- templates/service/_name.service.ts
+++ templates/service/_name.service.ts
@@ -2,10 +2,11 @@ import { Inject } from '<%= decoratorPath %>';
 import { BaseService } from './base.service';
 
 @Inject('$http', 'env')
-class <%= pName %>Service extends BaseService implements I<%= pName %>Service {
+export class <%= pName %>Service extends BaseService implements I<%= pName %>Service {
     constructor(http: ng.IHttpService, env: any) {
         super(http, env, 'prefix');
     }
 }
 
-export { <%= pName %>Service };
+export interface I<%= pName %>Service {
+}
