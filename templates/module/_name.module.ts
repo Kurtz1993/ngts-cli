b04274860c33be852cc5188649fae57fad47ce63
Luis Rogelio Hernández López <Kurtz@Luiss-MacBook-Pro.local>
2017-03-24 12:10:54 -0600

    Add empty line to default module template

--- templates/module/_name.module.ts
+++ templates/module/_name.module.ts
@@ -1,8 +1,8 @@
 <%if (!noImport) { %>import * as angular from 'angular';
 <% } %><% if (!moduleOnly) { %>import { <%= pName %>Config } from './<%= hName %>.config';
 import { <%= pName %>Controller } from './<%= hName %>.controller';
-
-<% } %>export const <%= pName %>Module = angular
+<% } %>
+export const <%= pName %>Module = angular
     .module('<%= appName %>.<%= name %>', [<% if (!moduleOnly) { %>'ui.router'])
     .controller('<%= pName %>Controller', <%= pName %>Controller)
     .config(<%= pName %>Config)<% } else { %>])<% } %>
