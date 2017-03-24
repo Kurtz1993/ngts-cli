077e8bb7f86a14f5405b14138b5fbebbb3222b18
Luis Rogelio Hernández López <Kurtz@Luiss-MacBook-Pro.local>
2017-03-24 14:42:53 -0600

    Use controller as string in component

--- templates/component/_name.component.ts
+++ templates/component/_name.component.ts
@@ -1,7 +1,5 @@
-import { <%= pName %>Controller } from './<%= hName %>.controller';
-
 export const <%= pName %>Component: ng.IComponentOptions = {
-    controller: <%= pName %>Controller,
+    controller: '<%= pName %>Controller',
     controllerAs: '<%= ctrlAlias %>',
     templateUrl: '<%= tplPath %><%= hName %>/<%= hName %>.tpl.html',
     bindings: {
