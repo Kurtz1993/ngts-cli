061b3b7cb068a9928e18aedf696956a8231ef1c0
Luis Rogelio Hernández López <Kurtz@Luiss-MacBook-Pro.local>
2017-04-06 15:33:17 -0500

    Adjust formating for entry module

--- templates/app/src/_name.ts
+++ templates/app/src/_name.ts
@@ -3,11 +3,12 @@ import { <%= pAppName %>Config } from './<%= hAppName %>.config';
 import { <%= pAppName %>Run } from './<%= hAppName %>.run';
 import { ServicesModule } from './services/services.module';
 
-angular.module('<%= appName %>', [
-    // Uncomment to use your app templates.
-    // '<%= appName %>.tpls',
-    'ui.router',
-    ServicesModule
-])
-.config(<%= pAppName %>Config)
-.run(<%= pAppName %>Run);
+angular
+    .module('<%= appName %>', [
+        // Uncomment to use your app templates.
+        // '<%= appName %>.tpls',
+        'ui.router',
+        ServicesModule
+    ])
+    .config(<%= pAppName %>Config)
+    .run(<%= pAppName %>Run);
